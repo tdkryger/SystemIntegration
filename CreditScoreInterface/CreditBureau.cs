92d8d5467a6786cb659e9973dce3582f8fa0a8d9
Thomas D. Kryger <tdkryger@akvariet.dk>
2015-12-02 13:17:52 +0100

    Credit Bureau Class library implemented

---
+++ CreditScoreInterface/CreditBureau.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CreditScoreInterface
+{
+    public static class CreditBureau
+    {
+        public static int GetCreditScore(string ssn)
+        {
+            var csi = new CreditScoreService.CreditScoreServiceClient();
+            int returnValue = csi.creditScore(ssn);
+            csi = null;
+            return returnValue;
+        }
+    }
+}
