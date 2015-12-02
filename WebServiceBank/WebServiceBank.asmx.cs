0d525418bcd58d253f1dc26bbc526e1953873f07
Thomas D. Kryger <tdkryger@akvariet.dk>
2015-12-02 11:32:15 +0100

    WebService Bank implementation

---
+++ WebServiceBank/WebServiceBank.asmx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServiceBank
+{
+    /// <summary>
+    /// Summary description for WebServiceBank
+    /// </summary>
+    [WebService(Namespace = "http://SharkLoans.com")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebServiceBank : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public double ProcessLoanRequest(string ssn, int creditScore, double amount, int duration)
+        {
+            return SimpleBank.Bank.ProcessLoanRequest(ssn, creditScore, amount, duration);
+        }
+    }
+}
