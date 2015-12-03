03f91b1c5ebd6c9c9edf8ed6907f85cb85c3383d
Thomas D. Kryger <tdkryger@akvariet.dk>
2015-12-03 09:17:54 +0100

    Test

--- CreditScoreInterface/CreditBureau.cs
+++ CreditScoreInterface/CreditBureau.cs
@@ -5,10 +5,14 @@ using System.Text;
 
 namespace CreditScoreInterface
 {
+    /// <summary>
+    /// blablabla
+    /// </summary>
     public static class CreditBureau
     {
         public static int GetCreditScore(string ssn)
         {
+           
             var csi = new CreditScoreService.CreditScoreServiceClient();
             int returnValue = csi.creditScore(ssn);
             csi = null;
