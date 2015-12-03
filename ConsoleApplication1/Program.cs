e634decc7e294828ce2263c234dbc55c69992810
Thomas D. Kryger <tdkryger@akvariet.dk>
2015-12-03 09:24:01 +0100

    Test nummer 386409

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿//using ConsoleApplication1.dk.cphbusiness.datdb;
+using ConsoleApplication1.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var nn = new CreditScoreService();
+            var bvb = new ConsoleApplication1.ServiceReference1.CreditScoreServiceClient();
+            bvb.creditScore("1234")
+        }
+    }
+}
