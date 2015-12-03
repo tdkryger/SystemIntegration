882a7ec3fa4797c752b855ef583029c29886999d
jacobege <jacob.ege@gmail.com>
2015-12-03 22:15:45 +0100

    Tja

--- RabbitMQBankTest/RabbitMQBankTest.cs
+++ RabbitMQBankTest/RabbitMQBankTest.cs
@@ -15,25 +15,25 @@ namespace RabbitMQBankTest
             Console.Write("-->Number of messages to send: ");
             int messages = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine("\n<--Started sending messages!");
+            Console.WriteLine("\n\t<--Started sending messages!");
             for (int i = 0; i < messages; i++)
             {
                 Utility.HandleMessaging.SendMessage<LoanRequest>(SendQueueName, new LoanRequest() { Amount = ((i + 1) * 10), CreditScore = 12, Duration = 12, SSN = "785637-1234" });
-                Console.WriteLine("<--Messages sent: " + (i + 1) + "/" + messages);
+                Console.WriteLine("\t<--Messages sent: " + (i + 1) + "/" + messages);
             }
-            Console.WriteLine("<--Stopped sending messages!\n");
+            Console.WriteLine("\t<--Stopped sending messages!\n");
 
             Console.Write("-->Press [Enter] to start receiving messages.");
             Console.ReadLine();
 
-            Console.WriteLine("\n<--Started receiving messages!");
+            Console.WriteLine("\n\t<--Started receiving messages!");
             Utility.HandleMessaging.RecieveMessage(ReceiveQueueName, delegate (BasicDeliverEventArgs ea)
             {
                 byte[] body = ea.Body;
                 string message = Encoding.UTF8.GetString(body);
-                Console.WriteLine("<--Message: " + message + " on queue: " + ea.RoutingKey);
+                Console.WriteLine("\t<--Message: " + message + " on queue: " + ea.RoutingKey);
             });
-            Console.WriteLine("<--Stopped receiving messages!\n");
+            Console.WriteLine("\t<--Stopped receiving messages!\n");
         }
     }
 }
