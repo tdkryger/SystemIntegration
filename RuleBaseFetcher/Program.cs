ce12cf05aa4253d06aae6260c933a93e635f4c46
jacobege <jacob.ege@gmail.com>
2015-12-05 18:49:31 +0100

    hey

---
+++ RuleBaseFetcher/Program.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuleBaseFetcher
+{
+    public class Program
+    {
+        private static string QUEUE_IN = "";
+        private static string QUEUE_OUT = "";
+
+        public static void Main(string[] args)
+        {
+            Utility.HandleMessaging.RecieveMessage(QUEUE_IN, (object model, BasicDeliverEventArgs ea) =>
+            {
+
+            });
+        }
+    }
+}
