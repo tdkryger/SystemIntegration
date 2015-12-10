814b3d1ba76a74fd3d66bbe13dd05eed333ca4f5
jacob.ege <jacob.ege@gmail.com>
2015-12-11 00:16:52 +0100

    Aggregating  bitch!

--- Aggregator/Program.cs
+++ Aggregator/Program.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using LoanBroker.model;
+using Newtonsoft.Json;
+using RabbitMQ.Client.Events;
+using System;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Aggregator
 {
     public class Program
     {
+        private static string QUEUE_IN = "group1_normalizer_out";
+        private static string QUEUE_OUT = "group1_aggregator_out";
+
         public static void Main(string[] args)
         {
+            LoanBroker.Utility.HandleMessaging.RecieveMessage(QUEUE_IN, (object model, BasicDeliverEventArgs ea) =>
+            {
+                Console.WriteLine("<--Message recieved on queue: " + QUEUE_IN);
+
+                LoanResponse loanResponse;
+
+                loanResponse = JsonConvert.DeserializeObject<LoanResponse>(Encoding.UTF8.GetString(ea.Body));
+
+                // Do some sweet aggregating!
+
+                Console.WriteLine("<--Sending message on queue: " + QUEUE_OUT);
+                Console.WriteLine();
+                LoanBroker.Utility.HandleMessaging.SendMessage<LoanResponse>(QUEUE_OUT, loanResponse);
+            });
         }
     }
 }
